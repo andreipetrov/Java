1adbfb0d16f6a3bf07bcee3198b77371b26b147b
Andrei Petrov <andreypetrov010@gmail.com>
2016-02-23 21:43:51 -0500

    Домашняя работа пункты 1-3

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -5,13 +5,22 @@ public class MyFirstProgram {
 		hello("world");
 		hello("user");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 9;
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Площадь прямогульника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-		System.out.println("Площадь прямогульника со сторонами " + a + " и " + b + " = " + area(a, b));
+		Point p1 = new Point();
+		p1.x = 5;
+		p1.y = 6;
+
+		Point p2 = new Point();
+		p2.x = 7;
+		p2.y = 8;
+
+		System.out.println("Расстояние между точкой P1 c координатами x = " + p1.x + " , y = " + p1.y +
+						" и точкой P2 c координатами x = " + p2.x + " , y = " + p2.y + " = " + distance(p1, p2));
 	}
 
 	public static void hello (String somebody) {
@@ -19,12 +28,8 @@ public class MyFirstProgram {
 
 	}
 
-	public static double area (double len) {
-		return len * len;
-	}
-
-	public static double area (double a, double b) {
-		return a * b;
+	public static double distance (Point p1, Point p2){
+		return Math.sqrt( (p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y) );
 	}
 
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,8 @@
+/**
+ * Created by andre on 23.02.2016.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+}
