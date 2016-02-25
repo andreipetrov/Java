f5322c701df711b5708909b0823cf242a2adea48
Andrei Petrov <andreypetrov010@gmail.com>
2016-02-24 22:18:53 -0500

    Домашнее задание часть 2. п4

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -7,7 +7,7 @@ public class MyFirstProgram {
 		Point p2 = new Point(7, 8);
 
 		System.out.println("Расстояние между точкой P1 c координатами x = " + p1.x + " , y = " + p1.y +
-						" и точкой P2 c координатами x = " + p2.x + " , y = " + p2.y + " = " + p.distance);
+						" и точкой P2 c координатами x = " + p2.x + " , y = " + p2.y + " = " + p1.distance(p2));
 	}
 
 }
