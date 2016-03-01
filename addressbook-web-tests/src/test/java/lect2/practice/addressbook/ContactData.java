65360548ec6e335c2cf15979468bab7faecd71f3
Andrei Petrov <andreypetrov010@gmail.com>
2016-02-29 23:46:17 -0500

    Тест с созданием контакта добавлен

---
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/ContactData.java
@@ -0,0 +1,79 @@
+package lect2.practice.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String ibitial;
+  private final String surname;
+  private final String position;
+  private final String companyTitle;
+  private final String address;
+  private final String mobilePhone;
+  private final String email;
+  private final String age;
+  private final String secondAddress;
+  private final String secondPhone;
+  private final String notes;
+
+  public ContactData(String name, String ibitial, String surname, String position, String companyTitle, String address, String mobilePhone, String email, String age, String secondAddress, String secondPhone, String notes) {
+    this.name = name;
+    this.ibitial = ibitial;
+    this.surname = surname;
+    this.position = position;
+    this.companyTitle = companyTitle;
+    this.address = address;
+    this.mobilePhone = mobilePhone;
+    this.email = email;
+    this.age = age;
+    this.secondAddress = secondAddress;
+    this.secondPhone = secondPhone;
+    this.notes = notes;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getIbitial() {
+    return ibitial;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public String getPosition() {
+    return position;
+  }
+
+  public String getCompanyTitle() {
+    return companyTitle;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobilePhone() {
+    return mobilePhone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public String getSecondAddress() {
+    return secondAddress;
+  }
+
+  public String getSecondPhone() {
+    return secondPhone;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+}
