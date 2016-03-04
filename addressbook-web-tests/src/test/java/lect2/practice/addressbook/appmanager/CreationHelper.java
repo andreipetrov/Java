62dde7623dde9b888cd24eb3cbc8d8f1ae5e43a3
Andrei Petrov <andreypetrov010@gmail.com>
2016-03-03 20:02:00 -0500

    Д.З. 5.4 - Класс HelperBase создан, методы перенесены

--- addressbook-web-tests/src/test/java/lect2/practice/addressbook/appmanager/CreationHelper.java
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/appmanager/CreationHelper.java
@@ -7,54 +7,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by andre on 02.03.2016.
  */
-public class CreationHelper {
-  private FirefoxDriver wd;
+public class CreationHelper extends HelperBase {
 
   public CreationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super (wd);
   }
 
   public void fillContactFrom(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(contactData.getIbitial());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(contactData.getPosition());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(contactData.getCompanyTitle());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(contactData.getAge());
-    wd.findElement(By.name("address2")).click();
-    wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys(contactData.getSecondAddress());
-    wd.findElement(By.name("phone2")).click();
-    wd.findElement(By.name("phone2")).clear();
-    wd.findElement(By.name("phone2")).sendKeys(contactData.getSecondPhone());
-    wd.findElement(By.name("notes")).click();
-    wd.findElement(By.name("notes")).clear();
-    wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
+    type(By.name("firstname"), contactData.getName());
+    type(By.name("middlename"), contactData.getIbitial());
+    ty(By.name("lastname"), contactData.getSurname());
+    name(By.name("title"), contactData.getPosition());
+    name(By.name("company"), contactData.getCompanyTitle());
+    name(By.name("address"), contactData.getAddress());
+    name(By.name("mobile"), contactData.getMobilePhone());
+    name(By.name("email"), contactData.getEmail());
+    name(By.name("byear"), contactData.getAge());
+    name(By.name("address2"), contactData.getSecondAddress());
+    name(By.name("phone2"), contactData.getSecondPhone());
+    name(By.name("notes"), contactData.getNotes());
     submitChanges();
   }
 
   public void submitChanges() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+
+   click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 }
--- addressbook-web-tests/src/test/java/lect2/practice/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/appmanager/NavigationHelper.java
@@ -6,18 +6,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by andre on 02.03.2016.
  */
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 
   public void addNewContact() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
