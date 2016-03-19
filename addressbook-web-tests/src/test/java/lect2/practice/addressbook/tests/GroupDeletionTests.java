9a0fa59b5cc39d4f2bada08a5ede30272668a6c9
Andrei Petrov <andreypetrov010@gmail.com>
2016-03-18 21:25:02 -0400

    Добавлена коллекции

--- addressbook-web-tests/src/test/java/lect2/practice/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/tests/GroupDeletionTests.java
@@ -4,6 +4,8 @@ import lect2.practice.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 /**
  * Created by andre on 03.03.2016.
  */
@@ -12,14 +14,14 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
-    if (! app.getGroupHelper().isThereAGroup()) {
+        if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test0", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteGroup();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
   }
 }
