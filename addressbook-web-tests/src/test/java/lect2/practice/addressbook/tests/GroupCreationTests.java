51fd9920746de71533e4d13f57d2d4cb58e4750b
Andrei Petrov <andreypetrov010@gmail.com>
2016-04-06 00:27:09 -0400

    6.11 - Логирование добавлено

--- addressbook-web-tests/src/test/java/lect2/practice/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,8 @@ import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import lect2.practice.addressbook.model.GroupData;
 import lect2.practice.addressbook.model.Groups;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
