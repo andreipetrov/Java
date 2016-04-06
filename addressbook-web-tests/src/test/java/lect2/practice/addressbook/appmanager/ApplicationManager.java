d905d93fb346750db8a7d9099b5fa4903d5f4ffb
Andrei Petrov <andreypetrov010@gmail.com>
2016-04-05 23:47:29 -0400

    ДЗ 16 - Тестовый стенд вынесен в конфигурационный файл

--- addressbook-web-tests/src/test/java/lect2/practice/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/appmanager/ApplicationManager.java
@@ -6,12 +6,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by andre on 02.03.2016.
  */
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
 
   private ContactHelper contactHelper;
@@ -21,11 +27,13 @@ public class ApplicationManager {
   private String browser;
 
   public ApplicationManager(String browser) {
-    System.out.println(browser);
     this.browser = browser;
+    properties = new Properties();
   }
 
-  public void init() {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
       if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)){
@@ -35,11 +43,11 @@ public class ApplicationManager {
     }
 
     wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     contactHelper = new ContactHelper(wd);
   }
 
--- addressbook-web-tests/src/test/java/lect2/practice/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/tests/TestBase.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+
 /**
  * Created by andre on 02.03.2016.
  */
-public abstract class TestBase {
-
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+public class TestBase {
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
