0582f7a7a6dd88da6720636cda1025840e5a8db2
Andrei Petrov <andreypetrov010@gmail.com>
2016-04-07 23:22:17 -0400

    7.4 - Сравнение групп в БД добавлена для тест модификации групп

--- addressbook-web-tests/src/test/java/lect2/practice/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/appmanager/ApplicationManager.java
@@ -25,6 +25,7 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private String browser;
+  private DBHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -34,6 +35,9 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    dbHelper = new DBHelper();
+
       if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)){
@@ -49,6 +53,7 @@ public class ApplicationManager {
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     contactHelper = new ContactHelper(wd);
+
   }
 
     public void stop() {
@@ -66,5 +71,7 @@ public class ApplicationManager {
   public ContactHelper contact() {
     return contactHelper;
   }
+
+  public DBHelper db() {return dbHelper;}
 
 }
