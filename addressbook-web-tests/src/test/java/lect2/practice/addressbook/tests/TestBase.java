67bf1ebc0c89c47eb7d43665f4e1b3fbfc2be186
Andrei Petrov <andreypetrov010@gmail.com>
2016-04-11 22:46:40 -0400

    7.5 - Отключаемые проверки UI для групп добавлены

--- addressbook-web-tests/src/test/java/lect2/practice/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/lect2/practice/addressbook/tests/TestBase.java
@@ -1,6 +1,9 @@
 package lect2.practice.addressbook.tests;
 
 import lect2.practice.addressbook.appmanager.ApplicationManager;
+import lect2.practice.addressbook.model.GroupData;
+import lect2.practice.addressbook.model.Groups;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +14,10 @@ import org.testng.annotations.BeforeSuite;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 /**
@@ -43,4 +50,13 @@ public class TestBase {
     logger.info("Stop test " + m.getName());
   }
 
+  public void verifyGroupListInUI() {
+    if (Boolean.getBoolean("verifyUI")) {
+      Groups dbGroups = app.db().groups();
+      Groups uiGroups = app.group().all();
+      assertThat(uiGroups, equalTo(dbGroups.stream()
+              .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
+  }
 }
