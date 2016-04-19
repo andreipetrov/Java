2fd1fe2f814ccef4804b213d2b4a63d4cbfd1ec0
Andrei Petrov <andreypetrov010@gmail.com>
2016-04-18 23:58:42 -0400

    8.6 - Тест регистрации добавлен

---
+++ mantis-tests/src/test/java/lect8/stqa/pft/mantis/model/MailMessage.java
@@ -0,0 +1,14 @@
+package lect8.stqa.pft.mantis.model;
+
+/**
+ * Created by andre on 18.04.2016.
+ */
+public class MailMessage {
+  public String to;
+  public String text;
+
+  public MailMessage(String to, String text) {
+    this.to = to;
+    this.text = text;
+  }
+}
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -5,3 +5,5 @@ $g_database_name = 'bugtracker';
 $g_database_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
